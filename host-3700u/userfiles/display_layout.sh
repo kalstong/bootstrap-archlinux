8e9456948cff72913c5955591073e8d165036c44
psevdaisthisi <code@fmoreira.art>
2022-04-22 12:54:09 +0100

    3700u: Fix office layout

--- host-3700u/userfiles/display_layout.sh
+++ host-3700u/userfiles/display_layout.sh
@@ -13,7 +13,7 @@ xrandr --output eDP --primary --mode 1920x1080 --pos 0x1440 --rotate normal --sc
        --output DisplayPort-2 --off
 
 elif [ "$1" = "office" ]; then
-xrandr --output eDP --primary --mode 1920x1080 --pos 0x1440 --rotate normal --scale 1 \
+xrandr --output eDP --primary --mode 1920x1080 --pos 320x1440 --rotate normal --scale 1 \
        --output DisplayPort-0 --mode 2560x1440 --pos 0x0 --rotate normal \
        --output DisplayPort-1 --off \
        --output DisplayPort-2 --off
