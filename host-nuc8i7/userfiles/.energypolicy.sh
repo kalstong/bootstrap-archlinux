ec76d1d4a195e2bcbd277355fc6dce4cf3c21b23
psevdaisthisi <code@fmoreira.art>
2022-04-27 13:24:29 +0100

    Normalize the layout of some energypolicy scripts

--- host-nuc8i7/userfiles/.energypolicy.sh
+++ host-nuc8i7/userfiles/.energypolicy.sh
@@ -6,14 +6,6 @@ if [ "$1" = "default" ]; then
 	sudo x86_energy_perf_policy normal &&
 	echo "$1" >> "$XDG_CONFIG_HOME/.energypolicy"
 
-elif [ "$1" = "performance" ]; then
-	sudo cpupower frequency-set --governor performance > /dev/null &&
-	sudo cpupower frequency-set --max "4.50GHz" > /dev/null &&
-	sudo cpupower set --perf-bias 5 > /dev/null &&
-	sudo intel_gpu_frequency --custom max="1200MHz" > /dev/null &&
-	sudo x86_energy_perf_policy performance &&
-	echo "$1" >> "$XDG_CONFIG_HOME/.energypolicy"
-
 elif [ "$1" = "balanced" ]; then
 	sudo cpupower frequency-set --governor powersave > /dev/null &&
 	sudo cpupower frequency-set --max "3.50GHz" > /dev/null &&
@@ -37,4 +29,13 @@ elif [ "$1" = "ultrapowersave" ]; then
 	sudo intel_gpu_frequency --custom max="600MHz" > /dev/null &&
 	sudo x86_energy_perf_policy power &&
 	echo "$1" >> "$XDG_CONFIG_HOME/.energypolicy"
+
+elif [ "$1" = "performance" ]; then
+	sudo cpupower frequency-set --governor performance > /dev/null &&
+	sudo cpupower frequency-set --max "4.50GHz" > /dev/null &&
+	sudo cpupower set --perf-bias 5 > /dev/null &&
+	sudo intel_gpu_frequency --custom max="1200MHz" > /dev/null &&
+	sudo x86_energy_perf_policy performance &&
+	echo "$1" >> "$XDG_CONFIG_HOME/.energypolicy"
+
 fi
