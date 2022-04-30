493bf1c559b1f8656cb1b734eb4683e89adc2f24
psevdaisthisi <code@fmoreira.art>
2022-05-01 00:54:00 +0100

    3700u: Update display layout profiles

--- host-3700u/userfiles/display_layout.sh
+++ host-3700u/userfiles/display_layout.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
 
 if [ "$1" = "solo" ]; then
-xrandr --output eDP --primary --mode 1920x1080 --pos 0x1440 --rotate normal --scale 1 \
+xrandr --output eDP --primary --mode 1920x1080 --pos 0x1440 --rotate normal \
        --output DisplayPort-0 --off \
        --output DisplayPort-1 --off \
        --output DisplayPort-2 --off
 
-elif [ "$1" = "home" ]; then
-xrandr --output eDP --primary --mode 1920x1080 --pos 0x1440 --rotate normal --scale 0.90 \
-       --output DisplayPort-0 --mode 2560x1440 --pos 1016x0 --rotate normal \
-       --output DisplayPort-1 --off \
-       --output DisplayPort-2 --off
-
 elif [ "$1" = "office" ]; then
-xrandr --output eDP --primary --mode 1920x1080 --pos 320x1440 --rotate normal --scale 1 \
-       --output DisplayPort-0 --mode 2560x1440 --pos 0x0 --rotate normal \
+xrandr --output eDP --primary --mode 1920x1080 --pos 0x0 --rotate normal \
+       --output DisplayPort-0 --mode 2560x1440 --pos 1920x0 --rotate normal \
        --output DisplayPort-1 --off \
        --output DisplayPort-2 --off
 fi
