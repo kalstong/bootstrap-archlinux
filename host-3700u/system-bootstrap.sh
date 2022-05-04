cb657f74b35cfb82f7c3d27a1f9be93d99e7f275
psevdaisthisi <code@fmoreira.art>
2022-05-04 15:21:35 +0100

    3700u: Fix specs description

--- host-3700u/system-bootstrap.sh
+++ host-3700u/system-bootstrap.sh
@@ -3,7 +3,7 @@
 # --- System Specs ---
 # HP EliteBook 745 G6 [1]
 # CPU: AMD Ryzen 7 PRO 3700U @2.30GHz [2]
-# RAM: 1x 8GiB DDR4 @2.667GHz
+# RAM: 2x 8GiB DDR4 @2.667GHz
 # STO: 512GB M.2 NVMe
 #
 # [1]: https://support.hp.com/us-en/document/c06386690
