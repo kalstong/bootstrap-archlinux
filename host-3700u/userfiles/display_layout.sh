4cd75d270a465c7c54c41e86f4057b1e7425a792
psevdaisthisi <code@fmoreira.art>
2022-05-04 14:22:57 +0100

    3700u: Update "dual" display_layout

--- host-3700u/userfiles/display_layout.sh
+++ host-3700u/userfiles/display_layout.sh
@@ -7,8 +7,11 @@ xrandr --output eDP --primary --mode 1920x1080 --pos 0x1440 --rotate normal \
        --output DisplayPort-2 --off
 
 elif [ "$1" = "dual" ]; then
-xrandr --output eDP --primary --mode 1920x1080 --pos 320x1440 --rotate normal \
-        --output DisplayPort-0 --mode 2560x1440 --pos 0x0 --rotate normal \
-        --output DisplayPort-1 --off \
-        --output DisplayPort-2 --off
+xrandr --output eDP --primary --mode 1920x1080 --pos 0x360 --rotate normal \
+       --output DisplayPort-0 --mode 2560x1440 --pos 1920x0 --rotate normal \
+       --output DisplayPort-1 --off \
+       --output DisplayPort-2 --off
 fi
+
+
+#!/bin/sh
