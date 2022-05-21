2781e223ecdc7ec53509851195b413e635c5ad22
psevdaisthisi <code@fmoreira.art>
2022-05-21 22:23:23 +0100

    8700k: Update energypolicy settings

--- host-8700k/userfiles/.energypolicy.sh
+++ host-8700k/userfiles/.energypolicy.sh
@@ -1,6 +1,6 @@
 if [ "$1" = "default" ]; then
 	sudo cpupower frequency-set --governor powersave > /dev/null &&
-	sudo cpupower frequency-set --max "4.00GHz" > /dev/null &&
+	sudo cpupower frequency-set --max "4.30GHz" > /dev/null &&
 	sudo cpupower set --perf-bias 6 > /dev/null &&
 	sudo intel_gpu_frequency --custom max="1000MHz" > /dev/null &&
 	sudo x86_energy_perf_policy normal &&
@@ -8,9 +8,9 @@ if [ "$1" = "default" ]; then
 
 elif [ "$1" = "balanced" ]; then
 	sudo cpupower frequency-set --governor powersave > /dev/null &&
-	sudo cpupower frequency-set --max "3.50GHz" > /dev/null &&
+	sudo cpupower frequency-set --max "3.70GHz" > /dev/null &&
 	sudo cpupower set --perf-bias 8 > /dev/null &&
-	sudo intel_gpu_frequency --custom max="900MHz" > /dev/null &&
+	sudo intel_gpu_frequency --custom max="1000MHz" > /dev/null &&
 	sudo x86_energy_perf_policy balance-power &&
 	echo "$1" >> "$XDG_CONFIG_HOME/.energypolicy"
 
@@ -32,7 +32,7 @@ elif [ "$1" = "ultrapowersave" ]; then
 
 elif [ "$1" = "performance" ]; then
 	sudo cpupower frequency-set --governor performance > /dev/null &&
-	sudo cpupower frequency-set --max "4.50GHz" > /dev/null &&
+	sudo cpupower frequency-set --max "4.70GHz" > /dev/null &&
 	sudo cpupower set --perf-bias 5 > /dev/null &&
 	sudo intel_gpu_frequency --custom max="1200MHz" > /dev/null &&
 	sudo x86_energy_perf_policy performance &&
