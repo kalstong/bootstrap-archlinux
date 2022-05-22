d1b1d766a4494653563e9e0016674348f1ffcfa7
psevdaisthisi <code@fmoreira.art>
2022-05-22 14:39:25 +0100

    8700k: Update energypolicy configuration

--- host-8700k/userfiles/.energypolicy.sh
+++ host-8700k/userfiles/.energypolicy.sh
@@ -2,9 +2,9 @@ if [ "$1" = "default" ]; then
 	sudo cpupower frequency-set --governor powersave > /dev/null &&
 	sudo cpupower frequency-set --max "4.30GHz" > /dev/null &&
 	sudo cpupower set --perf-bias 6 > /dev/null &&
-	sudo intel_gpu_frequency --custom max="1100MHz" > /dev/null &&
+	sudo intel_gpu_frequency --custom max="1200MHz" > /dev/null &&
 	sudo x86_energy_perf_policy normal &&
-	sudo nvidia-smi -pl 250 > /dev/null &&
+	sudo nvidia-smi -pl 215 > /dev/null &&
 	echo "$1" >> "$XDG_CONFIG_HOME/.energypolicy"
 
 elif [ "$1" = "balanced" ]; then
@@ -13,34 +13,32 @@ elif [ "$1" = "balanced" ]; then
 	sudo cpupower set --perf-bias 8 > /dev/null &&
 	sudo intel_gpu_frequency --custom max="1000MHz" > /dev/null &&
 	sudo x86_energy_perf_policy balance-power &&
-	sudo nvidia-smi -pl 200 > /dev/null &&
+	sudo nvidia-smi -pl 180 > /dev/null &&
 	echo "$1" >> "$XDG_CONFIG_HOME/.energypolicy"
 
 elif [ "$1" = "powersave" ]; then
 	sudo cpupower frequency-set --governor powersave > /dev/null &&
 	sudo cpupower frequency-set --max "2.90GHz" > /dev/null &&
 	sudo cpupower set --perf-bias 10 > /dev/null &&
-	sudo intel_gpu_frequency --custom max="700MHz" > /dev/null &&
+	sudo intel_gpu_frequency --custom max="600MHz" > /dev/null &&
 	sudo x86_energy_perf_policy balance-power &&
-	sudo nvidia-smi -pl 150 > /dev/null &&
+	sudo nvidia-smi -pl 130 > /dev/null &&
 	echo "$1" >> "$XDG_CONFIG_HOME/.energypolicy"
 
 elif [ "$1" = "ultrapowersave" ]; then
 	sudo cpupower frequency-set --governor powersave > /dev/null &&
 	sudo cpupower frequency-set --max "1.50GHz" > /dev/null &&
 	sudo cpupower set --perf-bias 15 > /dev/null &&
-	sudo intel_gpu_frequency --custom max="600MHz" > /dev/null &&
+	sudo intel_gpu_frequency --custom max="400MHz" > /dev/null &&
 	sudo x86_energy_perf_policy power &&
 	sudo nvidia-smi -pl 105 > /dev/null &&
 	echo "$1" >> "$XDG_CONFIG_HOME/.energypolicy"
 
 elif [ "$1" = "performance" ]; then
-	# @NOTE: Setting the GPU frequency to the defaults so maybe the hardware
-	# can then assume the BIOS overclocking settings of 1.3GHz.
 	sudo cpupower frequency-set --governor performance > /dev/null &&
 	sudo cpupower frequency-set --max "4.70GHz" > /dev/null &&
 	sudo cpupower set --perf-bias 5 > /dev/null &&
-	sudo intel_gpu_frequency --defaults > /dev/null &&
+	sudo intel_gpu_frequency --max > /dev/null &&
 	sudo x86_energy_perf_policy performance &&
 	sudo nvidia-smi -pl 280 > /dev/null &&
 	echo "$1" >> "$XDG_CONFIG_HOME/.energypolicy"
