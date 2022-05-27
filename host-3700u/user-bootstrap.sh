d9b07d9253d3d8ee8ac693fe1cbd1a8e17931775
psevdaisthisi <code@fmoreira.art>
2022-05-27 18:20:02 +0100

    Install Go Version Manager in all hosts

--- host-3700u/user-bootstrap.sh
+++ host-3700u/user-bootstrap.sh
@@ -88,6 +88,13 @@ pip3 install --user flashfocus pynvim pywal
 sudo -H pip3 install vpn-slice
 
 printinfo "\n"
+printinfo "+ ----------------------------- +"
+printinfo "| Installing Go Version Manager |"
+printinfo "+ ----------------------------- +"
+[ "$bt_stepping" ] && { yesno "Continue?" || exit 1; }
+. ../shared/userfiles/install-g.sh
+
+printinfo "\n"
 printinfo "+ ------------------------------------ +"
 printinfo "| Installing NVM, NodeJS, NPM and Yarn |"
 printinfo "+ ------------------------------------ +"
