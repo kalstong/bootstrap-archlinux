73534b43e7a6fd726dfb9e4041a56b744a40b9b3
psevdaisthisi <code@fmoreira.art>
2022-06-06 11:39:01 +0100

    nuc8hvk: Update display layout

--- host-nuc8hvk/userfiles/display_layout.sh
+++ host-nuc8hvk/userfiles/display_layout.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 if [ "$1" = "dual" ]; then
-xrandr --output HDMI-A-3 --off \
-       --output HDMI-A-4 --off \
-       --output DisplayPort-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
-       --output DisplayPort-3 --mode 2560x1440 --pos 0x0 --rotate normal --left-of DisplayPort-2 \
-       --output DisplayPort-4 --off \
-       --output DisplayPort-5 --off
+xrandr \
+	--output DisplayPort-0 --off \
+	--output DisplayPort-1 --off \
+	--output DisplayPort-2 --primary --mode 2560x1440 --pos 1440x168 --rotate normal \
+	--output DisplayPort-3 --mode 2560x1440 --pos 0x0 --rotate left \
+	--output HDMI-A-0 --off \
+	--output HDMI-A-1 --off
 fi
+
+#!/bin/sh
