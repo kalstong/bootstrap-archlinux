8d7c333075775833e7a625b7d1b2eab9906e52f2
psevdaisthisi <code@fmoreira.art>
2022-07-08 23:22:47 +0100

    scripts: Update FVM to v2.4.1 and cleanup code

--- shared/userfiles/install-fvm.sh
+++ shared/userfiles/install-fvm.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-fvm_ver="2.4.0";
+set -e
+
+fvm_ver="2.4.1";
 fvm_pkg="https://github.com/fluttertools/fvm/releases/download/${fvm_ver}/fvm-${fvm_ver}-linux-x64.tar.gz";
 
 rm -rf /tmp/fvm*
 
-echo "Downloading FVM v$fvm_ver ..." &&
+echo "==> Downloading FVM v$fvm_ver ..."
 curl -sS --connect-timeout 13 --retry 5 --retry-delay 2 \
 	-H 'Accept:application/vnd.github.v3.raw' \
-	-L "$fvm_pkg" -o /tmp/fvm.tar.gz &&
+	-L "$fvm_pkg" -o /tmp/fvm.tar.gz
 
-echo "Extracting FVM v$fvm_ver ..." &&
-tar fvxz /tmp/fvm.tar.gz -C /tmp/ --wildcards 'fvm/fvm' --strip-components=1 &&
+echo "==> Installing FVM ..."
+tar fvxz /tmp/fvm.tar.gz -C /tmp/ --wildcards 'fvm/fvm' --strip-components=1
 mv /tmp/fvm "${HOME}/.local/bin/"
-
 rm -rf /tmp/fvm*
+
+echo "==> Done."
