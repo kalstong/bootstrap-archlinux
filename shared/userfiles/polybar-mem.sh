be36f52dbbf040bde0b280c0e56eae30781b7f64
psevdaisthisi <code@fmoreira.art>
2022-07-10 11:27:30 +0100

    scripts: Account for non-reclaimable and tmpfs mem

--- shared/userfiles/polybar-mem.sh
+++ shared/userfiles/polybar-mem.sh
@@ -8,7 +8,9 @@ do
 	buffers=$(echo "$info" | grep '^Buffers:' | awk '{print $2}')
 	cached=$(echo "$info" | grep '^Cached:' | awk '{print $2}')
 	reclaimable=$(echo "$info" | grep '^SReclaimable:' | awk '{print $2}')
-	used=$((total - free - buffers - cached - reclaimable))
+	unreclaimable=$(echo "$info" | grep '^SUnreclaimable:' | awk '{print $2}')
+	tmpfs=$(echo "$info" | grep '^Shmem:' | awk '{print $2}')
+	used=$((total - free - buffers - cached - reclaimable + unreclaimable + tmpfs))
 	echo $(echo "scale=3; $used / 1024 / 1024" | bc)GiB
 	sleep $1
 done
