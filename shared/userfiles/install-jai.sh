a78944f11b1056f00b7735461edf8ff85f8335dd
psevdaisthisi <code@fmoreira.art>
2022-07-11 18:12:39 +0100

    scripts: Improve JAI installer

--- shared/userfiles/install-jai.sh
+++ shared/userfiles/install-jai.sh
@@ -5,22 +5,27 @@ set -e
 ver="0.1.031"
 url="https://www.dropbox.com/s/4lfvceze2x8ra95/jai-beta-1-031.zip?dl=1"
 
-rm -rf /tmp/jai*
+[ ! -v JAI_DIR ] && echo "==> ERROR: \$JAI_DIR is not defined." && exit 1
+
+
+rm -rf /tmp/jai/ /tmp/jai.zip
 echo "==> Downloading JAI v$ver ..."
-curl --connect-timeout 13 --retry 5 --retry-delay 2 "$url" \
-	-L -sS -o /tmp/jai.zip
+curl --connect-timeout 13 --location --progress-bar --retry 5 --retry-delay 2 \
+	"$url" --output /tmp/jai.zip
 
 echo "==> Installing JAI ..."
 unzip /tmp/jai.zip -d /tmp > /dev/null
 
-rm -rf "$JAI_DIR"/*
+[ -d "$JAI_DIR" ] && rm -rf "$JAI_DIR"
 mkdir -p "$JAI_DIR"
 
 mv /tmp/jai/* "$JAI_DIR"
-rm -rf "$JAI_DIR"/bin/{jai.exe,lld-macosx,lld.exe}
 mv "$JAI_DIR"/bin/jai-linux "$JAI_DIR"/bin/jai
+
+rm -rf "$JAI_DIR"/bin/{jai.exe,lld-macosx,lld.exe}
 chmod u+x "$JAI_DIR"/bin/{jai,lld-linux}
 
-rm -rf /tmp/jai*
+echo "==> Removing temporary files ..."
+rm -rf /tmp/jai/ /tmp/jai.zip
 
 echo "==> Done."
