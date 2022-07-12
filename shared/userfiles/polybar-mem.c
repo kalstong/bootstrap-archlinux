3a7a04bfc32343fae62eae5d38dfe7c7f73990f3
psevdaisthisi <code@fmoreira.art>
2022-07-12 10:30:18 +0100

    polybar: Fix memory plugin and simplify loop

--- shared/userfiles/polybar-mem.c
+++ shared/userfiles/polybar-mem.c
@@ -28,51 +28,56 @@ int main(int argc, char **argv)
 	FILE *stream = fopen("/proc/meminfo", "r");
 	if (stream == NULL) return -1;
 
+	#define TOTAL_SOURCES 7
 	unsigned int mem_total = 0, mem_free = 0, mem_buffers = 0,
 		mem_cached = 0, mem_reclaimable = 0, mem_unreclaimable = 0,
-		mem_tmpfs = 0;
+		mem_tmpfs = 0, i = 0;
 
 	char *line = 0;
 	size_t len = 0;
-	while (getline(&line, &len, stream) != -1) {
-		if (mem_total == 0 && starts_with(line, "MemTotal:")) {
+	while (i < TOTAL_SOURCES) {
+		getline(&line, &len, stream);
+		if (starts_with(line, "MemTotal:")) {
+			i++;
 			sscanf(line, "MemTotal: %u kB", &mem_total);
 			continue;
 		}
 
-		if (mem_free == 0 && starts_with(line, "MemFree:")) {
+		if (starts_with(line, "MemFree:")) {
+			i++;
 			sscanf(line, "MemFree: %u kB", &mem_free);
 			continue;
 		}
 
-		if (mem_buffers == 0 && starts_with(line, "Buffers:")) {
+		if (starts_with(line, "Buffers:")) {
+			i++;
 			sscanf(line, "Buffers: %u kB", &mem_buffers);
 			continue;
 		}
 
-		if (mem_cached == 0 && starts_with(line, "Cached:")) {
+		if (starts_with(line, "Cached:")) {
+			i++;
 			sscanf(line, "Cached: %u kB", &mem_cached);
 			continue;
 		}
 
-		if (mem_reclaimable == 0 && starts_with(line, "SReclaimable:")) {
+		if (starts_with(line, "SReclaimable:")) {
+			i++;
 			sscanf(line, "SReclaimable: %u kB", &mem_reclaimable);
 			continue;
 		}
 
-		if (mem_unreclaimable == 0 && starts_with(line, "SUnreclaimable:")) {
-			sscanf(line, "SUnreclaimable: %u kB", &mem_unreclaimable);
+		if (starts_with(line, "SUnreclaim:")) {
+			i++;
+			sscanf(line, "SUnreclaim: %u kB", &mem_unreclaimable);
 			continue;
 		}
 
-		if (mem_tmpfs == 0 && starts_with(line, "Shmem:")) {
+		if (starts_with(line, "Shmem:")) {
+			i++;
 			sscanf(line, "Shmem: %u kB", &mem_tmpfs);
 			continue;
 		}
-
-		if (mem_total > 0 && mem_free > 0 && mem_cached > 0 &&
-		    mem_reclaimable > 0 && mem_reclaimable > 0 && mem_tmpfs > 0)
-			break;
 	}
 
 	char msg[16] = {0};
