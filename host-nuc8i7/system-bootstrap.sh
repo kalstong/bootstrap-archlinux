68c4835718e1da6d5596c0f73cf3108ee3d8ba81
psevdaisthisi <code@fmoreira.art>
2022-08-09 11:43:40 +0100

    pacman: Add code pkg to 3700u and nuc8i7

--- host-nuc8i7/system-bootstrap.sh
+++ host-nuc8i7/system-bootstrap.sh
@@ -191,8 +191,9 @@ pacman_development=(
 	python-pip tokei unixodbc vulkan-icd-loader vulkan-mesa-layers
 )
 pacman_apps=(
-	alacritty arandr chromium drawing feh libreoffice-still maim mpv obs-studio
-	pavucontrol peek remmina rofi signal-desktop slock sxiv virt-viewer
+	alacritty arandr chromium code drawing feh libreoffice-still maim mpv
+	obs-studio pavucontrol peek remmina rofi signal-desktop slock sxiv
+	virt-viewer
 )
 pacman_fonts=(
 	noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra terminus-font
