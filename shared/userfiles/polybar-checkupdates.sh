d901d41e2a34210f3a9f69a9a495d7b91a61896d
psevdaisthisi <code@fmoreira.art>
2022-08-27 20:09:54 +0100

    polybar: Fix retcode validation of checkupdates.sh

--- shared/userfiles/polybar-checkupdates.sh
+++ shared/userfiles/polybar-checkupdates.sh
@@ -3,8 +3,8 @@
 updates=$(checkupdates 2> /dev/null)
 ret=$?
 
-if [ $ret -eq 0 ]; then
-	echo $(echo -e "$updates" | wc -l)
+if [ $ret -eq 0 ] || [ $ret -eq 2 ]; then
+	echo $(printf "%s" "$updates" | wc -l)
 else
 	echo "?"
 fi
