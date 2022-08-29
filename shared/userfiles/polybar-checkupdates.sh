5965a7baa9950d2545ba0d40150aeec166f425a2
psevdaisthisi <code@fmoreira.art>
2022-08-29 18:33:20 +0100

    polybar: Fix off-by-one error in checkupdates plugin

--- shared/userfiles/polybar-checkupdates.sh
+++ shared/userfiles/polybar-checkupdates.sh
@@ -4,7 +4,8 @@ updates=$(checkupdates 2> /dev/null)
 ret=$?
 
 if [ $ret -eq 0 ] || [ $ret -eq 2 ]; then
-	echo $(printf "%s" "$updates" | wc -l)
+	[ -n "$updates" ] && echo $(printf "%s\n" "$updates" | wc -l) && exit 0
+	[ -z "$updates" ] && echo "0" && exit 0
 else
 	echo "?"
 fi
