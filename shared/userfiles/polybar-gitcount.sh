b0bb672c8e94bae4401288947038ec64ed72aad7
psevdaisthisi <code@fmoreira.art>
2022-09-22 11:47:21 +0100

    misc: Fix polybar-gitcount.sh

--- shared/userfiles/polybar-gitcount.sh
+++ shared/userfiles/polybar-gitcount.sh
@@ -5,8 +5,10 @@ args="$2"
 
 out=$(/usr/bin/git -C "$dir" $args 2> /dev/null)
 ret=$?
-if [ $ret -eq 0 ]; then
-	echo $(printf "%s" "$out" | wc -l)
+if [ $ret -eq 0 ] && [ -n "$out" ]; then
+	echo "$out" | wc -l
+elif [ $ret -eq 0 ] ; then
+	echo "0"
 else
 	echo "?"
 fi
