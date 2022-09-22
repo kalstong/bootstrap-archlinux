f7b423f5ba94649561154476ec47f8214cb0dfb2
psevdaisthisi <code@fmoreira.art>
2022-09-22 12:19:35 +0100

    config: Use inotifywait in polybar-gitcount
    This way we can trigger the script only where occur git-related changes.

--- shared/userfiles/polybar-gitcount.sh
+++ shared/userfiles/polybar-gitcount.sh
@@ -3,12 +3,18 @@
 dir="$1"
 args="$2"
 
-out=$(/usr/bin/git -C "$dir" $args 2> /dev/null)
-ret=$?
-if [ $ret -eq 0 ] && [ -n "$out" ]; then
-	echo "$out" | wc -l
-elif [ $ret -eq 0 ] ; then
-	echo "0"
-else
-	echo "?"
-fi
+/usr/bin/inotifywait \
+	--monitor "${dir}/.git" --recursive \
+	--event create --event modify 2> /dev/null |
+	while read directory action file; do
+		# echo "'directory'/'$file' via '$action'"
+		out=$(/usr/bin/git -C "$dir" $args 2> /dev/null)
+		ret=$?
+		if [ $ret -eq 0 ] && [ -n "$out" ]; then
+			echo "$out" | wc -l
+		elif [ $ret -eq 0 ] ; then
+			echo "0"
+		else
+			echo "?"
+		fi
+	done
