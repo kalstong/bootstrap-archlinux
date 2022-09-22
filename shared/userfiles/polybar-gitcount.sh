4f4a68b77d9937fc878a295a6629db49e27ade7e
psevdaisthisi <code@fmoreira.art>
2022-09-22 11:42:58 +0100

    misc: Simplify polybar-gitcount.sh

--- shared/userfiles/polybar-gitcount.sh
+++ shared/userfiles/polybar-gitcount.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/dash
 
-args="$1"
-dir="$2"
+dir="$1"
+args="$2"
 
-pushd "$dir" > /dev/null
-out=$(/usr/bin/git $args 2> /dev/null)
+out=$(/usr/bin/git -C "$dir" $args 2> /dev/null)
 ret=$?
 if [ $ret -eq 0 ]; then
 	echo $(printf "%s" "$out" | wc -l)
 else
 	echo "?"
 fi
-popd > /dev/null
