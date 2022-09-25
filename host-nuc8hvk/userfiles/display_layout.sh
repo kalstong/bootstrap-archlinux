395a8e6c223087f44386eadf50848930b5f9cdf4
psevdaisthisi <code@fmoreira.art>
2022-09-25 21:57:59 +0100

    config: Fix nuc8hvk's dual screen layout

--- host-nuc8hvk/userfiles/display_layout.sh
+++ host-nuc8hvk/userfiles/display_layout.sh
@@ -16,6 +16,6 @@ xrandr \
 	--output DisplayPort-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
 	--output DisplayPort-3 --off \
 	--output HDMI-A-0 --off \
-	--output HDMI-A-1 --right-of DisplayPort-2 --mode 2560x1440 --pos 0x0 --rotate normal
+	--output HDMI-A-1 --left-of DisplayPort-2 --mode 2560x1440 --pos 0x0 --rotate normal
 fi
 
