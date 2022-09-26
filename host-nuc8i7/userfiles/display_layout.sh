5cb7af3ab2f9d5114f7856f98e0a171697ef5c42
psevdaisthisi <code@fmoreira.art>
2022-09-26 01:22:08 +0100

    config: Fix nuc8i7 dual display layout

--- host-nuc8i7/userfiles/display_layout.sh
+++ host-nuc8i7/userfiles/display_layout.sh
@@ -9,7 +9,7 @@ xrandr \
 
 elif [ "$1" = "dual" ]; then
 xrandr \
-	--output DP1 --mode 2560x1440 --pos 2560x0 --rotate normal \
+	--output DP1 --mode 2560x1440 --pos 2560x0 --rotate left \
 	--output DP2 --primary --mode 2560x1440 --pos 0x254 --rotate normal \
 	--output HDMI1 --off \
 	--output VIRTUAL1 --off
